52b73198a7aaebfebc3acba0eab2ecee20d44ab5
Tony Tabone <atabone@ecornell.com>
2019-06-24 22:47:04 -0400

    moving materials into new locations

--- CIS533/Source Material/LogLoss/helper.py
+++ CIS533/Source Material/LogLoss/helper.py
@@ -1,4 +1,17 @@
 import numpy as np
+import traceback
+import sys
+
+def runtest(test,name):
+    print('Running Test: %s ... ' % (name),end='')
+    try:
+        if test():
+            print('✔ Passed!')
+        else:
+            print("✖ Failed!\n The output of your function does not match the expected output. Check your code and try again.")
+    except Exception as e:
+        print('✖ Failed!\n Your code raises an exception. The following is the traceback of the failure:')
+        print(' '.join(traceback.format_tb(sys.exc_info()[2])))
 
 
 def log_likelihood_grader(X, y, w):
